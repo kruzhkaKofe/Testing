ec7c58a9c8eb22d26ef6fbe211687c8ea57e58d5
kanishev <kanishevnikita@yandex.kz>
2025-12-07 15:15:32 +0300

    test: fix 2 теста

--- tasks/second.ts
+++ tasks/second.ts
@@ -1,17 +1,19 @@
 export interface PriceData {
-  id: string;
-  priceUsd: string;
+    id: string;
+    priceUsd: string;
 }
 
 export const getTokenPrice = async (tokenId: string): Promise<number> => {
-  try {
-    const response = await fetch(`https://api.coincap.io/v2/assets/${tokenId}`);
-    if (!response.ok) {
-      throw new Error('Network error');
+    try {
+        const response = await fetch(
+            `https://api.coincap.io/v2/assets/${tokenId}`,
+        );
+        if (!response.ok) {
+            throw new Error("Network error");
+        }
+        const data = await response.json();
+        return parseFloat(data.data.priceUsd);
+    } catch (e) {
+        return 0;
     }
-    const data = await response.json();
-    return parseFloat(data.data.priceUsd);
-  } catch (e) {
-    throw e;
-  }
 };
