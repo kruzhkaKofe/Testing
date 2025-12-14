e1870089a66689a132cd3324185b01c1ccbe9bd9
kanishev <kanishevnikita@yandex.kz>
2025-12-14 15:58:41 +0300

    add vue support

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <h1>Hello Vue in Vite!</h1>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const message = ref("Vue is now running in your existing Vite project.");
+</script>
+
+<style scoped>
+.container {
+    font-family: sans-serif;
+    padding: 20px;
+}
+</style>
